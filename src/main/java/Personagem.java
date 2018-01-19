9ca5868b1cb20baad86934a842ceca028096c8c3
MrDallOca <tdalloca@gmail.com>
2018-01-19 14:12:47 -0200

    TesterPersonagem

--- src/main/java/Personagem.java
+++ src/main/java/Personagem.java
@@ -2,9 +2,35 @@ public class Personagem {
     int vida;
     int forca;
 
+    public int getAgilidade() {
+        return agilidade;
+    }
+
+    public void setAgilidade(int agilidade) {
+        this.agilidade = agilidade;
+    }
+
+    public int getEnergia() {
+        return energia;
+    }
+
+    public void setEnergia(int energia) {
+        this.energia = energia;
+    }
+
+    int agilidade;
+    int energia;
+
+
     public Personagem(int vida, int forca) {
         this.setVida(vida);
         this.setForca(forca);
+    }
+
+    public Personagem(int forca, int agilidade, int energia) {
+        this.setForca(forca);
+        this.setAgilidade(agilidade);
+        this.setEnergia(energia);
     }
 
     public void recebeAtaque(Personagem personagem) {
--- src/test/java/PersonagemTeste.java
+++ src/test/java/PersonagemTeste.java
@@ -1,2 +1,10 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class PersonagemTeste {
+    @Test
+    public void testeCalculoVida() {
+        Personagem p1 = new Personagem(1 , 1 ,1);
+        Assert.assertEquals(p1.getVida(), 1.5, 0);//1.5
+    }
 }
